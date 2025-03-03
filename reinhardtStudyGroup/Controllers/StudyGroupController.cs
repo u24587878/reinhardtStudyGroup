16b70d04e0dceba7723022f615655f111510176b
u24587878 <reinhardt.coetzee2005@gmail.com>
2025-03-03 18:40:38 +0200

    mvc

---
+++ reinhardtStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace reinhardtStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult Index()
+        {
+            List<Models.GroupMemberClass> people = new List<Models.GroupMemberClass>();
+
+            people.Add(new Models.GroupMemberClass { StudentNumber = "u24587878", FirstName = "Reinhardt", 
+                LastName = "Coetzee", EmailAddress = "u24587878@gmail.com"});
+
+            people.Add(new Models.GroupMemberClass
+            {
+                StudentNumber = "u22499475",
+                FirstName = "Reece",
+                LastName = "Wenhold",
+                EmailAddress = "u22499475@gmail.com"
+            });
+
+            people.Add(new Models.GroupMemberClass
+            {
+                StudentNumber = "u24711919",
+                FirstName = "Regardt",
+                LastName = "van Heerden",
+                EmailAddress = "u24711919@gmail.com"
+            });
+
+            people.Add(new Models.GroupMemberClass
+            {
+                StudentNumber = "u23658772",
+                FirstName = "Xander",
+                LastName = "Visagie",
+                EmailAddress = "u23658772@gmail.com"
+            });
+
+            people.Add(new Models.GroupMemberClass
+            {
+                StudentNumber = "u24664929",
+                FirstName = "Arno",
+                LastName = "Smalberger",
+                EmailAddress = "u24664929@gmail.com"
+            });
+
+            return View(people);
+        }
+    }
+}
---
+++ reinhardtStudyGroup/Models/GroupMemberClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace reinhardtStudyGroup.Models
+{
+    public class GroupMemberClass
+    {
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+    }
+}
